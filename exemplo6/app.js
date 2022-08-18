fc47c9c7bd30795b32524f28bc4c78d0ddba701d
Ignacio Neto <ignacio.cunha.neto@gmail.com>
2022-08-18 11:46:15 -0300

    1

--- exemplo6/app.js
+++ exemplo6/app.js
@@ -1,12 +1,28 @@
 import express from "express";
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Uma resposta simpática');
+    // res.send('Uma resposta simpática3');
+    res.render('paginas/index');
 
+});
+
+app.get('/sobre', (req, res) => {
+    // res.send('Sobre');
+    res.render('paginas/sobre');
+});
+
+app.get('/contato', (req, res) => {
+    // res.send('Contato');
+    res.render('paginas/contato');
 });
 
 // executando o servidor
 app.listen(8080, () => {
     console.log('Servidor express rodando...');
 });
+// Obs Ctrl + C (Para o servidor)
+
+// preparar para receber arquivos html
+// instalação do EJS
